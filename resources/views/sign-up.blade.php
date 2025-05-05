fa17490cac8db539e25c0221a2c3a571ac889d56
unknown <intanramadanii88@gmail.com>
2025-05-05 15:56:54 +0700

    replace frontend sign-in and sign-up

---
+++ resources/views/sign-up.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="/css/app.css">
+    <title>Transnet Sumbar</title>
+</head>
+<body class="w-dvw h-dvh flex items-center">
+    <div class="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-4xl">
+        <div class="hidden bg-cover lg:block lg:w-1/2 bg-contain" style="background-image: url('/assets/img/hero4.jpeg')"></div>
+    
+        <div class="w-full px-6 py-8 md:px-8 lg:w-1/2">
+            <form class="w-full">
+                <div class="flex justify-center mx-auto">
+                    <img class="w-auto h-7 sm:h-8" src="/assets/img/logo.png" alt="">
+                </div>
+    
+                <p class="mt-3 text-xl text-center text-gray-600 dark:text-gray-200">
+                    Create your account
+                </p>
+    
+                <div class="relative flex items-center mt-8">
+                    <span class="absolute">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                        </svg>
+                    </span>
+                    <input type="text" placeholder="Username" class="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40">
+                </div>
+    
+                <div class="relative flex items-center mt-6">
+                    <span class="absolute">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                        </svg>
+                    </span>
+                    <input type="email" placeholder="Email address" class="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40">
+                </div>
+    
+                <div class="relative flex items-center mt-4">
+                    <span class="absolute">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+                        </svg>
+                    </span>
+                    <input type="password" placeholder="Password" class="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40">
+                </div>
+    
+                <div class="relative flex items-center mt-4">
+                    <span class="absolute">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+                        </svg>
+                    </span>
+                    <input type="password" placeholder="Confirm Password" class="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40">
+                </div>
+    
+                <div class="mt-6">
+                    <button class="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
+                        Sign Up
+                    </button>
+    
+                    <div class="mt-6 text-center">
+                        <a href="sign-in" class="text-sm text-gray-500 hover:underline dark:text-gray-400">
+                            Already have an account?
+                        </a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,11 @@ use App\Http\Controllers\ContentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[ContentController::class, 'index']);
+
+Route::get('/sign-in',function(){
+    return view('sign-in');
+});
+
+Route::get('/sign-up',function(){
+    return view('sign-up');
+});

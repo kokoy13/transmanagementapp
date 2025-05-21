cee9128d295020e9eb50fb5b5430842704653f9f
kokoy13 <andikafiransyah1905@gmail.com>
2025-05-22 01:45:41 +0700

    feat: Menambahkan Menu Cek Order

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -78,7 +78,9 @@
                         <div data-aos="fade-down" x-data="{ open: false }" class="relative">
                             <img @click="open = !open" src="{{ Auth::user()->avatar }}" alt="Avatar" class="rounded-full w-10 h-10 cursor-pointer">
                             <div x-show="open" @click.away="open = false" class="absolute right-0 mt-2 w-48 bg-white rounded shadow-lg z-10">
-                                <a href="/dashboard" class="block px-4 py-2 hover:bg-gray-100">Dashboard</a>
+                                <a href="/profile" class="block px-4 py-2 hover:bg-gray-100">Profile</a>
+                                <a href="/check-order" class="block px-4 py-2 hover:bg-gray-100">My Orders</a>
+                                <a href="/" class="block px-4 py-2 hover:bg-gray-100">Notification</a>
                                 <form method="POST" action="{{ route('logout') }}">
                                     @csrf
                                     <button type="submit" class="block w-full text-left px-4 py-2 hover:bg-gray-100">Logout</button>
